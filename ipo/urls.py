163a6ac555d83e32afa191ebf3a969870cd53672
unknown <rushankrushank51@gmail.com>
2024-07-24 16:46:19 +0530

    Resolve merge conflicts

--- ipo/urls.py
+++ ipo/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('sharkinvestor', views.sharkinvestor, name='sharkinvestor'),
     path('Allbrokers' ,views.Allbrokers, name='Allbrokers'),
     path('BrokerCompare' ,views.BrokerCompare,name='BrokerCompare'),
+    path('active_clients' ,views.active_clients,name='active_clients'),
+     path('charges' ,views.charges,name='charges'),
 ]
